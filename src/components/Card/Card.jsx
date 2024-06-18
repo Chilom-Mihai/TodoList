5070e4d9de09e0cb26138566b3cda6ff74c01262
Mihai Chilom <mihai.chilom@yahoo.com>
2024-06-18 21:35:10 +0300

    edit task functionality added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
 import "./Card.css";
 import Input from "../Input/Input";
+import EditInput from "../EditInput/EditInput";
 import Todo from "../Todo/Todo";
+import { v4 as uuidv4 } from "uuid";
 
 const Card = () => {
   const [todoArray, setTodoarray] = useState([]);
   const addTodo = (todo) => {
     //face spread si adauga si nou todo
-    setTodoarray([...todoArray, todo]);
+    setTodoarray([
+      ...todoArray,
+      { id: uuidv4(), todoDesc: todo, isEditting: false },
+    ]);
   };
   console.log(todoArray);
+
+  const deleteTodo = (id) => {
+    setTodoarray(todoArray.filter((todo) => todo.id !== id));
+  };
+  const editTodoMode = (id) => {
+    setTodoarray(
+      todoArray.map((todo) =>
+        todo.id === id ? { ...todo, isEditting: !todo.isEditting } : todo
+      )
+    );
+  };
   return (
     <div className="card">
       <h1 className="text-[2.7rem] font-semibold pb-5 text-center text-white">
@@ -20,9 +36,24 @@ const Card = () => {
         buttonText="Add Task"
         addTodo={addTodo}
       />
-      {todoArray.map((todo, index) => (
-        <Todo key={index} todoText={todo} />
-      ))}
+      {todoArray.map((todo, index) =>
+        todo.isEditting ? (
+          <EditInput
+            placeholderText="Edit your Task"
+            buttonText="Edit Task"
+            addTodo={addTodo}
+            key={index}
+          />
+        ) : (
+          <Todo
+            key={index}
+            todoText={todo.todoDesc}
+            deleteTask={deleteTodo}
+            id={todo.id}
+            editTask={editTodoMode}
+          />
+        )
+      )}
       {/* <Input placeholderText="Edit your task" buttonText="Edit Task" /> */}
       {/* <Input placeholderText="View your task" buttonText="View Task" /> */}
       {/* <Todo todoText="Task2" />
